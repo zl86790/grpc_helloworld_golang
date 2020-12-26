41349155abb3a5336c8d54cb274dc86db1aa479f
lizhe <marshal_li_b@163.com>
2020-12-26 09:12:20 -0800

    ip

--- services/HiService.go
+++ services/HiService.go
@@ -2,13 +2,63 @@ package services
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"net"
 )
 
 type HiService struct {
 }
 
+func externalIP() (net.IP, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return nil, err
+		}
+		for _, addr := range addrs {
+			ip := getIpFromAddr(addr)
+			if ip == nil {
+				continue
+			}
+			return ip, nil
+		}
+	}
+	return nil, errors.New("connected to the network?")
+}
+
+func getIpFromAddr(addr net.Addr) net.IP {
+	var ip net.IP
+	switch v := addr.(type) {
+	case *net.IPNet:
+		ip = v.IP
+	case *net.IPAddr:
+		ip = v.IP
+	}
+	if ip == nil || ip.IsLoopback() {
+		return nil
+	}
+	ip = ip.To4()
+	if ip == nil {
+		return nil // not an ipv4 address
+	}
+
+	return ip
+}
+
 func (hs HiService) GetHiResponed(ctx context.Context, request *HiRequest) (*HiResponse, error) {
 	fmt.Println("Say:" + request.Say)
-	return &HiResponse{Responed: "helloworld"}, nil
+	ip, _ := externalIP()
+
+	return &HiResponse{Responed: " helloworld " + ip.To4().String()}, nil
 }
